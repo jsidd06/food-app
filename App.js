27134c1f5a0a8723c738c680fa40e408c65d7f69
siddharth <siddharth.wonderpillars@gmail.com>
2022-05-01 23:29:47 +0530

    add navigator bar using some pachage and setup some bugs while install

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StyleSheet, StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import CategoryScreen from "./screens/CategoryScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+import "react-native-gesture-handler";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <CategoryScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Meals Categories" component={CategoryScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
