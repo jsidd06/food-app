aad53b78d549d4e2754306d5694240c0f607abd1
siddharth <siddharth.wonderpillars@gmail.com>
2022-05-05 08:31:39 +0530

    add a data to new screen with details data without style

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,8 +1,29 @@
-import { Text } from "react-native";
+import { Text, View, Image } from "react-native";
+import MealDetail from "../components/MealDetail";
+import { MEALS } from "../data/dummy-data";
 
 function MealDetailsScreen({ route }) {
   const mealId = route.params.mealId;
-  return <Text>MealDetailsScreen({mealId})</Text>;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  return (
+    <View>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+      <Text>{selectedMeal.title}</Text>
+      <MealDetail
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+      />
+      <Text>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+      <Text>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <Text key={step}>{step}</Text>
+      ))}
+    </View>
+  );
 }
 
 export default MealDetailsScreen;
